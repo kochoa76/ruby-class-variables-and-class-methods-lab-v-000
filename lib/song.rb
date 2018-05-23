f295a9665cb631a91ec0a42edfcbb3095da59e9b
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-23 21:45:29 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song 
+  attr_accessor :song_name, :song_artist, :song_genre
+  
+  def initialize(song_name, song_artist, song_genre)
+    @song_name = song_name
+    @song_artist= song_artist 
+    @song_genre= song_genre 
+  end 
+  
+  
+end 
