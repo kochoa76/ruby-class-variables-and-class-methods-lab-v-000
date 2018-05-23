f9b72308a38f10c5f07b443a0464ea13429b03ba
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-23 22:45:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,8 +37,18 @@ class Song
  end 
   genre_count 
 end 
-    
-    
+
+def self.artist_count 
+  artist_count = {}
+  @@artists.each do |artist|
+    if artist.uniq 
+      artist_count[artist]+=1 
+    else
+      artist_count[artist] = 1 
+    end 
+  end 
+  artist_count 
+end 
     
     
     
