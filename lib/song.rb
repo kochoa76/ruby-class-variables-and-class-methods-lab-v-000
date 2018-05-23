4b78bea9e4df4a1bbb97b96c069d55ba5d3becec
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-23 21:49:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,22 @@
 class Song 
   attr_accessor :song_name, :song_artist, :song_genre
   
+  @@count= 0 
+  
+  def self.count 
+    @@count
+  end 
+  
+  def self.genres 
+    existing_songs = []
+    existing_songs << if @song_name.include?(song_name) 
+    
+  
   def initialize(song_name, song_artist, song_genre)
     @song_name = song_name
     @song_artist= song_artist 
     @song_genre= song_genre 
+    @@count += 1 
   end 
   
   
