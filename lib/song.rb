f789528e696b5f92ef86a6ed78fdb29ac2483dcd
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-23 22:43:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,9 +30,9 @@ class Song
     genre_count ={}
    @@genres.each do |genre|
      if genre_count[genre]  
-       genre_count +=1 
+       genre_count[genre] +=1 
      else 
-       genre_count = 1 
+       genre_count[genre] = 1 
    end 
  end 
   genre_count 
